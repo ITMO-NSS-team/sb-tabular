3564577363b74406adfce67367dcf6e0724a588e
V0rtexyz <409024@edu.itmo.ru>
2026-03-04 15:50:55 +0300

    add bosting mode for LighSB-M

---
+++ sbtab/solvers/ipf_lightsbm_boosted/__init__.py
@@ -0,0 +1,4 @@
+from sbtab.solvers.ipf_lightsbm_boosted.config import LightSBMBoostedConfig
+from sbtab.solvers.ipf_lightsbm_boosted.solver import LightSBMBoostedSolver
+
+__all__ = ["LightSBMBoostedConfig", "LightSBMBoostedSolver"]
