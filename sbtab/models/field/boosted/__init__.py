6880897551a5327f4b72d3a100d2b1ca54a14fdb
Vortexx <159257067+V0rtexyz@users.noreply.github.com>
2026-03-11 06:40:27 +0300

    Remove imports and __all__ from __init__.py

--- sbtab/models/field/boosted/__init__.py
+++ sbtab/models/field/boosted/__init__.py
@@ -1,4 +1,1 @@
-from sbtab.models.field.boosted.lgbm import LGBMStepConfig, LGBMStepModel
-from sbtab.models.field.boosted.per_step import PerStepField
 
-__all__ = ["LGBMStepConfig", "LGBMStepModel", "PerStepField"]
